411d773a7384b2641c36a51c5557ec80f9c0f3d5
MingAo.Wang <490746560@qq.com>
2020-10-22 11:44:56 +0800

    新增侧滑栏

--- app/src/main/java/com/wma/fun/login/LoginActivity.java
+++ app/src/main/java/com/wma/fun/login/LoginActivity.java
@@ -1,14 +1,26 @@
 package com.wma.fun.login;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.wma.fun.R;
+import com.wma.fun.databinding.ActivityLoginBinding;
 import com.wma.library.base.BaseActivity;
 
 /**
  * create by wma
  * on 2020/10/21 0021
  */
-public class LoginActivity extends BaseActivity {
-    @Override
+public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//    }
+
+        @Override
     public String getTitleStr() {
         return null;
     }
@@ -20,6 +32,6 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.activity_login;
     }
 }
